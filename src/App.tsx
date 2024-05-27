edde2d3311f67a3cf7fe8075f64891508200561b
Daniel Marttins <daniel.marttinsx@gmail.com>
2024-05-27 17:18:24 -0300

    Create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="flex item-center justify-center w-screen h-screen bg-slate-900">
-      
+    <div className="flex item-center justify-center w-screen h-screen" style={{ background: '#3B5875' }}>
+      <Router>
+            <Routes>
+              <Route path="/" element={<Home />} /*element={logado ? <Navigate to="/home/main" /> : <Login />}*/ />
+              {/*<Route path="home" element={<Home />}  /*element={logado ? <Home /> : <Navigate to="/" />} >
+                <Route path="main" element={<Main />} />
+                <Route path="company" element={<Company />} />
+                <Route path="persons" element={<Persons />} />
+                <Route path="responsible" element={<Responsible />} />
+                <Route path="order" element={<Movement />} />
+                <Route path="paymentMethods" element={<PaymentMethods />} />
+                <Route path="expenses" element={<Expense />} />
+                <Route path="assets" element={<Asset />} />
+                <Route path="groups" element={<Groups />} />
+                <Route path="situations" element={<Situation />} />
+                <Route path="contracts" element={<Contract />} />
+              </Route>*/}
+            </Routes>
+        </Router>
     </div>
   );
 }
