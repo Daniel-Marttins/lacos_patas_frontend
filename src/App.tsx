bc73d56d4ec504078c0e5b2953485c5c3ffb85ca
NOXVOIDIA <97589454+Daniel-Marttins@users.noreply.github.com>
2024-05-27 14:35:25 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex item-center justify-center w-screen h-screen bg-slate-900">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx,html}', './public/index.html'],
+  darkMode: false, // or 'edia' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
